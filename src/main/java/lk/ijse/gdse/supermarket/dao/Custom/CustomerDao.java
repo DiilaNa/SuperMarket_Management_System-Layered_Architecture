237050769a2b9cb914dda2605a1b0e1af5a6b386
DiilaNa <liyanaarachchidilan@gmail.com>
2025-02-24 16:44:43 +0530

    interface CrudDAO added

--- src/main/java/lk/ijse/gdse/supermarket/dao/Custom/CustomerDao.java
+++ src/main/java/lk/ijse/gdse/supermarket/dao/Custom/CustomerDao.java
@@ -1,4 +1,8 @@
 package lk.ijse.gdse.supermarket.dao.Custom;
 
-public interface CustomerDao {
+import lk.ijse.gdse.supermarket.dao.CrudDAO;
+import lk.ijse.gdse.supermarket.entity.Customer;
+
+public interface CustomerDao extends CrudDAO<Customer> {
+
 }
--- src/main/java/lk/ijse/gdse/supermarket/dao/Custom/Impl/CustomerDaoImpl.java
+++ src/main/java/lk/ijse/gdse/supermarket/dao/Custom/Impl/CustomerDaoImpl.java
@@ -1,4 +1,43 @@
 package lk.ijse.gdse.supermarket.dao.Custom.Impl;
 
-public class CustomerDaoImpl {
+import lk.ijse.gdse.supermarket.dao.Custom.CustomerDao;
+import lk.ijse.gdse.supermarket.entity.Customer;
+
+import java.util.ArrayList;
+
+public class CustomerDaoImpl implements CustomerDao {
+    @Override
+    public ArrayList<Customer> getALL() {
+        return null;
+    }
+
+    @Override
+    public boolean save(Customer dto) {
+        return false;
+    }
+
+    @Override
+    public void update(Customer dto) {
+
+    }
+
+    @Override
+    public void delete(Customer dto) {
+
+    }
+
+    @Override
+    public boolean exist(String id) {
+        return false;
+    }
+
+    @Override
+    public String generateNewId() {
+        return "";
+    }
+
+    @Override
+    public Customer search(String id) {
+        return null;
+    }
 }
